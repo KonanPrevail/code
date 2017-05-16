bb6d6bee8663692ea6cab34f46d9d50d892ddd1e
Cuong Do <cuong.do@uconn.edu>
2017-05-15 20:20:26 -0400

    Data Mining Applications

---
+++ data mining applications (.NET)/DecisionTreeCreditClassification/DecisionTreeCreditClassification/DecisionTreeCreditClassification/CreditChecker.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CreditChecker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.cboCheckingAccount = New System.Windows.Forms.ComboBox()
+        Me.cboCreditHistory = New System.Windows.Forms.ComboBox()
+        Me.cboPresentEmployment = New System.Windows.Forms.ComboBox()
+        Me.cboJob = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cboForeigner = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(445, 50)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 0
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'cboCheckingAccount
+        '
+        Me.cboCheckingAccount.FormattingEnabled = True
+        Me.cboCheckingAccount.Location = New System.Drawing.Point(151, 24)
+        Me.cboCheckingAccount.Name = "cboCheckingAccount"
+        Me.cboCheckingAccount.Size = New System.Drawing.Size(189, 21)
+        Me.cboCheckingAccount.TabIndex = 1
+        '
+        'cboCreditHistory
+        '
+        Me.cboCreditHistory.FormattingEnabled = True
+        Me.cboCreditHistory.Location = New System.Drawing.Point(151, 51)
+        Me.cboCreditHistory.Name = "cboCreditHistory"
+        Me.cboCreditHistory.Size = New System.Drawing.Size(189, 21)
+        Me.cboCreditHistory.TabIndex = 2
+        '
+        'cboPresentEmployment
+        '
+        Me.cboPresentEmployment.FormattingEnabled = True
+        Me.cboPresentEmployment.Location = New System.Drawing.Point(151, 78)
+        Me.cboPresentEmployment.Name = "cboPresentEmployment"
+        Me.cboPresentEmployment.Size = New System.Drawing.Size(121, 21)
+        Me.cboPresentEmployment.TabIndex = 3
+        '
+        'cboJob
+        '
+        Me.cboJob.FormattingEnabled = True
+        Me.cboJob.Location = New System.Drawing.Point(151, 105)
+        Me.cboJob.Name = "cboJob"
+        Me.cboJob.Size = New System.Drawing.Size(121, 21)
+        Me.cboJob.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Checking Account"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(76, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Credit History"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 81)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(131, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Present Employment since"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(121, 108)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(24, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Job"
+        '
+        'cboForeigner
+        '
+        Me.cboForeigner.FormattingEnabled = True
+        Me.cboForeigner.Location = New System.Drawing.Point(151, 132)
+        Me.cboForeigner.Name = "cboForeigner"
+        Me.cboForeigner.Size = New System.Drawing.Size(121, 21)
+        Me.cboForeigner.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(94, 135)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Foreigner"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(303, 135)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(37, 13)
+        Me.lblResult.TabIndex = 11
+        Me.lblResult.Text = "Result"
+        '
+        'CreditChecker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(569, 192)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.cboForeigner)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cboJob)
+        Me.Controls.Add(Me.cboPresentEmployment)
+        Me.Controls.Add(Me.cboCreditHistory)
+        Me.Controls.Add(Me.cboCheckingAccount)
+        Me.Controls.Add(Me.btnRun)
+        Me.Name = "CreditChecker"
+        Me.Text = "Credit Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnRun As System.Windows.Forms.Button
+    Friend WithEvents cboCheckingAccount As System.Windows.Forms.ComboBox
+    Friend WithEvents cboCreditHistory As System.Windows.Forms.ComboBox
+    Friend WithEvents cboPresentEmployment As System.Windows.Forms.ComboBox
+    Friend WithEvents cboJob As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents cboForeigner As System.Windows.Forms.ComboBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+
+End Class
---
+++ data mining applications (.NET)/DecisionTreeCreditClassification/DecisionTreeCreditClassification/DecisionTreeCreditClassification/CreditChecker.vb
@@ -0,0 +1,126 @@
+﻿Imports numl.Model
+Imports System
+Imports System.IO
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports numl.Supervised.DecisionTree
+Public Class CreditChecker
+    ' Only 5 attributes selected
+    Public TrainingData As New List(Of Applicant)
+    Public oCheckingAccount As New List(Of String)
+    Public oCreditHistory As New List(Of String)
+    Public oPresentEmployment As New List(Of String)
+    Public oJob As New List(Of String)
+    Public oForeigner As New List(Of String)
+    Dim ApplicantDescriptor As Descriptor
+    Dim decisionTreeGenerator
+    Dim decisionTreeModel
+    Dim fileLineContents = ""
+    Dim ApplicantAttributes
+    Dim oApplicant As Applicant
+
+    Public Sub LoadData(ByVal trainingDataFile As String)
+        If Not File.Exists(trainingDataFile) Then
+            Throw New FileNotFoundException()
+        End If
+        Using fileReader As StreamReader = New StreamReader(New FileStream(trainingDataFile, FileMode.Open))
+            fileLineContents = fileReader.ReadLine()
+            Do Until fileLineContents Is Nothing
+                ApplicantAttributes = fileLineContents.Split(" "c)
+                oApplicant = New Applicant()
+
+                oApplicant.CheckingAccount = ApplicantAttributes(0)
+                oApplicant.CreditHistory = ApplicantAttributes(2)
+                oApplicant.PresentEmployment = ApplicantAttributes(6)
+                oApplicant.Job = ApplicantAttributes(16)
+                oApplicant.Foreigner = ApplicantAttributes(19)
+
+                If ApplicantAttributes(20) = 1 Then
+                    oApplicant.CreditClass = Applicant.CreditTypes.Good
+                Else
+                    oApplicant.CreditClass = Applicant.CreditTypes.Bad
+                End If
+                TrainingData.Add(oApplicant)
+                fileLineContents = fileReader.ReadLine()
+            Loop
+        End Using
+    End Sub
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        Dim oTestApplicant = New Applicant()
+        With oTestApplicant
+            .CheckingAccount = oCheckingAccount(cboCheckingAccount.SelectedIndex)
+            .CreditHistory = oCreditHistory(cboCreditHistory.SelectedIndex)
+            .PresentEmployment = oPresentEmployment(cboPresentEmployment.SelectedIndex)
+            .Job = oJob(cboJob.SelectedIndex)
+            .Foreigner = oForeigner(cboForeigner.SelectedIndex)
+        End With
+
+        Dim iDecision = decisionTreeModel.Predict(oTestApplicant)
+        If iDecision.CreditClass = Applicant.CreditTypes.Good Then
+            lblResult.Text = "Result: Approved"
+        Else
+            lblResult.Text = "Result: Disapproved"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cboCheckingAccount.Items.Add("< 0")
+        cboCheckingAccount.Items.Add("0 <= ... < 200 DM")
+        cboCheckingAccount.Items.Add(">= 200")
+        cboCheckingAccount.Items.Add("no checking account")
+        cboCheckingAccount.SelectedIndex = 1
+        oCheckingAccount.Add("A11")
+        oCheckingAccount.Add("A12")
+        oCheckingAccount.Add("A13")
+        oCheckingAccount.Add("A14")
+
+        cboCreditHistory.Items.Add("no credits taken/ all credits paid back duly")
+        cboCreditHistory.Items.Add("all credits at this bank paid back duly ")
+        cboCreditHistory.Items.Add("existing credits paid back duly till now")
+        cboCreditHistory.Items.Add("delay in paying off in the past")
+        cboCreditHistory.Items.Add("critical account/ other credits existing (not at this bank)")
+        cboCreditHistory.SelectedIndex = 1
+        oCreditHistory.Add("A30")
+        oCreditHistory.Add("A31")
+        oCreditHistory.Add("A32")
+        oCreditHistory.Add("A33")
+        oCreditHistory.Add("A34")
+
+        cboPresentEmployment.Items.Add("unemployed")
+        cboPresentEmployment.Items.Add("< 1 year")
+        cboPresentEmployment.Items.Add("1 <= ... < 4 years")
+        cboPresentEmployment.Items.Add("4 <= ... < 7 years")
+        cboPresentEmployment.Items.Add("... >= 7 years")
+        cboPresentEmployment.SelectedIndex = 1
+        oPresentEmployment.Add("A91")
+        oPresentEmployment.Add("A92")
+        oPresentEmployment.Add("A93")
+        oPresentEmployment.Add("A94")
+        oPresentEmployment.Add("A95")
+
+        cboJob.Items.Add("unemployed/ unskilled - non-resident")
+        cboJob.Items.Add("unskilled - resident")
+        cboJob.Items.Add("skilled employee / official")
+        cboJob.Items.Add("management/ self-employed/highly qualified employee/ officer")
+        cboJob.SelectedIndex = 1
+        oJob.Add("A171")
+        oJob.Add("A172")
+        oJob.Add("A173")
+        oJob.Add("A174")
+
+        cboForeigner.Items.Add("yes")
+        cboForeigner.Items.Add("no")
+        cboForeigner.SelectedIndex = 1
+        oForeigner.Add("A201")
+        oForeigner.Add("A202")
+
+        ApplicantDescriptor = Descriptor.Create(Of Applicant)()
+        decisionTreeGenerator = New DecisionTreeGenerator(ApplicantDescriptor)
+        LoadData("C:\Users\MC\Documents\gcredit.txt")
+        decisionTreeModel = decisionTreeGenerator.Generate(TrainingData)
+    End Sub
+
+End Class
